f2edad4d15775bd8dbbd1b97768a1fffb7aaa7e5
Luis Fuentes <LDFG1002@gmail.com>
2023-03-14 22:18:41 -0500

    Declaracion de variables globales en config.php

---
+++ app/config/config.php
@@ -0,0 +1,35 @@
+<?php
+    // Website
+    # Application path: folder 'app'
+    define('PATH_APP',  dirname(dirname(__FILE__)));
+    # URL path
+    define('PATH_URL',  'http://localhost/MVC');
+    # Website title
+    define('TITLE',     'Diseño MVC');
+    # Website description
+    define('DESCRIP',   'Breve descripción del proyecto');
+    # Website authors
+    define('AUTHOR',    'Nombre de los desarrolladores del sitio');
+
+    // Database
+    # Database host
+    define('HOST',      'localhost');
+    # Database username
+    define('USER',      'root');
+    # Database password
+    define('PSWD',      '');
+    # Database name
+    define('DB',        'mvc');
+
+    // Mailer
+    # Sender password
+    define('PSWD_SMTP', 'xxxxxxxxxxxxxxxx');
+    # Sender email
+    define('USER_SMTP', 'LDFG1002@gmail.com');
+    # Sender name
+    define('NAME_SMTP', 'MVC proofs');
+    # Mailer server port
+    define('PORT_SMTP', 465);
+    # Mailer server host
+    define('HOST_SMTP', 'smtp.gmail.com');
+?>
